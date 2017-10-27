f64fde213aa933a005e836092416d034670a5bfa
Daniel Faria <daniel@imac-do-daniel-faria2.local>
2017-10-27 16:06:23 -0200

    Removing unwanted language stylecode for intellij

--- java/intellij/install-code-style-on-intelij.sh
+++ java/intellij/install-code-style-on-intelij.sh
@@ -3,7 +3,7 @@
 echo "atualizando arquivos de padrões de codificação..."
 for n in ~/Library/Preferences/IntelliJIdea*/codestyles/
 do
-	cp intellij-java-google-style.xml $n
-	echo "copiando para $n"	
+	cp intellij-java-bluesoft-style.xml $n
+	echo "copiando para $n"
 done
-echo $'\e[32m' "Para finalizar, inicie o Intellij e vá em Preferencias > CodeStyle e selecione o scheme GoogleStyle." $'\e[0m'
+echo $'\e[32m' "Para finalizar, inicie o Intellij e vá em Preferencias > CodeStyle e selecione o scheme BluesoftStyle." $'\e[0m'
